enadam/giccs

commit 31200ca16cc54f46244354d2323a3386a6611823

giccs: expand environment variables in CmdFTP.split()
Respecting escaping and quotation.

giccs.py
@@ -7021,7 +7021,42 @@ class CmdFTP(CmdExec, CommonOptions, DownloadBlobOptions,
 		token = None
 		quoted = None
 		escaped = False
+		environ = environ_delimited = None
 		for c in s:
+			if environ is not None:
+				assert not escaped
+
+				isalnum = re.match(r'\w', c)
+				if isalnum:
+					environ += c
+				elif not environ and c == '{':
+					assert not environ_delimited
+					environ_delimited = True
+				elif environ_delimited and c != '}':
+					raise ValueError(
+						"%s: illegal character in "
+						"environment variable name"
+						% repr(c))
+				elif not environ:
+					raise ValueError(
+						"Empty environment variable "
+						"name")
+				else:
+					val = os.environ.get(environ)
+					if val is None:
+						raise ValueError(
+							f"{environ}: no such "
+							"environment variable")
+
+					if token is None:
+						token = val
+					else:
+						token += val
+					environ = None
+
+				if isalnum or environ_delimited:
+					continue
+
 			if escaped:
 				assert token is not None
 				if c == '\\' or c in Globber.GLOBBING:
@@ -7029,6 +7064,9 @@ class CmdFTP(CmdExec, CommonOptions, DownloadBlobOptions,
 				else:
 					token += c
 				escaped = False
+			elif c == '$' and quoted != "'":
+				environ = ""
+				environ_delimited = False
 			elif quoted is not None:
 				assert token is not None
 				if c == quoted:
@@ -7057,7 +7095,28 @@ class CmdFTP(CmdExec, CommonOptions, DownloadBlobOptions,
 			raise ValueError("Dangling escape character")
 		elif quoted is not None:
 			raise ValueError("Unclosed quotation")
-		elif token is not None:
+
+		if environ is not None:
+			if environ_delimited:
+				raise ValueError(
+					f"{environ}: unfinished environment "
+					"variable name")
+			elif not environ:
+				raise ValueError(
+					"Empty environment variable name")
+
+			val = os.environ.get(environ)
+			if val is None:
+				raise ValueError(
+					f"{environ}: no such environment "
+					"variable")
+
+			if token is None:
+				token = val
+			else:
+				token += val
+
+		if token is not None:
 			yield token
 
 	def execute(self) -> None:

commit 2d8df65808a0e332fdfd8c5f20c99b234613f61c

giccs: set the $STATUS environment variable in the FTP shell
Which can be printed with the echo command now.  This supersedes the "$?"
hack, which was broken by 31200ca16cc54f46244354d2323a3386a6611823.

giccs.py
@@ -7127,6 +7127,12 @@ class CmdFTP(CmdExec, CommonOptions, DownloadBlobOptions,
 		error = False
 		re_shell = re.compile(r"^\s*!(.*)")
 		while True:
+			# Setting the environment variable may leak memory,
+			# so only do that if @error has changed.
+			error_str = str(int(error))
+			if os.environ.get("STATUS") != error_str:
+				os.environ["STATUS"] = error_str
+
 			try:
 				line = input("%s> " % self.remote.cwd.path())
 			except KeyboardInterrupt:
@@ -7153,9 +7159,6 @@ class CmdFTP(CmdExec, CommonOptions, DownloadBlobOptions,
 			if cmdline[0].startswith('-'):
 				print(f"Invalid command.", file=sys.stderr)
 				error = True
-				continue
-			if cmdline == [ "$?" ]:
-				print(int(error))
 				continue
 
 			try: